c1a60b5618a6fdac37fb8e5297cc7121daaa815d
Roberto Delgado <roberto.delgado@gmail.com>
2020-09-03 18:07:38 -1000

    Added error handling for GET req

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -6,31 +6,41 @@ const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function PhotoList() {
   const [photo, setPhoto] = useState([]);
+  const [noPhoto, setnoPhoto] = useState({});
 
   const didUpdate = () => {
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
       .then((response) => {
         setPhoto(response.data);
-        console.log("rd: didUpdate res:", response);
+        // console.log("rd: didUpdate res:", response);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => setnoPhoto(error.message));
   };
 
   useEffect(didUpdate, []);
 
-  if (!photo) return <div>No photo right now, please check back later</div>;
+  let error = JSON.stringify(noPhoto);
 
   return (
     <div className="photo">
-      <PhotoCard
-        photoURL={photo.url}
-        photoMedia={photo.media_type}
-        photoTitle={photo.title}
-        photoDate={photo.date}
-        photoDescription={photo.explanation}
-        photoCopyright={photo.copyright}
-      />
+      {noPhoto ? (
+        <>
+          <p>{error}</p>
+          <p>
+            Please come back later. The NASA photo of the day is not ready yet.
+          </p>
+        </>
+      ) : (
+        <PhotoCard
+          photoURL={photo.url}
+          photoMedia={photo.media_type}
+          photoTitle={photo.title}
+          photoDate={photo.date}
+          photoDescription={photo.explanation}
+          photoCopyright={photo.copyright}
+        />
+      )}
     </div>
   );
 }
