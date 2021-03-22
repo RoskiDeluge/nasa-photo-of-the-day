2cee3c1b64ac3fb5b1d0155412d2ed742c57a2e0
RoskiDeluge <roberto.delgado@gmail.com>
2021-03-22 11:07:58 -1000

    Changed app title

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <AppTitle>NASA Photo of The Day</AppTitle>
+        <AppTitle>SFM</AppTitle>
         <Route component={PhotoList} path="/" exact />
         <AppFooter>
           <MusicContainer>
